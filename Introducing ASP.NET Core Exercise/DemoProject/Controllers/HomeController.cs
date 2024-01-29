cab2e71e3c6cec9f029e48b2a4ba2c0a129cb225
Alex Markov <langhorne05@gmail.com>
2024-01-29 08:58:36 +0200

    Create HomeController.cs

---
+++ Introducing ASP.NET Core Exercise/DemoProject/Controllers/HomeController.cs
@@ -0,0 +1,39 @@
+﻿using DemoProject.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+
+namespace DemoProject.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            ViewBag.Message = "This is Demo ASP.Net MVC app.";
+            return View();
+        }
+
+        public IActionResult About()
+        {
+            ViewBag.Message = "This is Demo ASP.Net MVC app.";
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
