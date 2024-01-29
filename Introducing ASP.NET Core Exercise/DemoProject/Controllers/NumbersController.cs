358cba218af703d5e77f37b0d2273689331f37d2
Alex Markov <langhorne05@gmail.com>
2024-01-29 08:58:45 +0200

    Create NumbersController.cs

---
+++ Introducing ASP.NET Core Exercise/DemoProject/Controllers/NumbersController.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace DemoProject.Controllers
+{
+    public class NumbersController : Controller
+    {
+        private readonly ILogger<NumbersController> _logger;
+
+        public NumbersController(ILogger<NumbersController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpGet]
+        public IActionResult GetNums(int num)
+        {
+            return View(num);
+        }
+
+    }
+}
